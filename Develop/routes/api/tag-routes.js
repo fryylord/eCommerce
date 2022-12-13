668278382aba6012d7a1eaee9f08f35380f56d0a
fryylord <rob@mchays.com>
2022-12-13 14:10:12 -0600

    added post to tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -41,8 +41,14 @@ router.get('/:id', async(req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
