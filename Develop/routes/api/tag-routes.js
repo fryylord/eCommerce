6a7c9c9722725fd9d02dcf76692ac58e0fd64aa2
fryylord <rob@mchays.com>
2022-12-13 14:11:06 -0600

    added put in tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -51,8 +51,23 @@ router.post('/', async(req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body,
+    {
+      where: {
+        id:req.params.id
+      }
+    }
+    );
+    if(!updatedTag[0]) {
+      res.status(404).json({message: "No tag with that id is found"});
+    }
+    res.status(200).json(updatedTag);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.delete('/:id', (req, res) => {
