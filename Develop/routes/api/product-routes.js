bc946e93b3e540905c1f110f8b3a34b9a7ff7944
fryylord <rob@mchays.com>
2022-12-13 14:08:59 -0600

    added get's to tag-routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -134,8 +134,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedProduct) {
+      res.status(404).json({
+        message: "No product found with this id!"
+      })
+    }
+    res.status(200).json(deletedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
