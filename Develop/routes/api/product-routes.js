9e8ea581787b721c68be23da4608bfe5ffc4564b
fryylord <rob@mchays.com>
2022-12-16 08:46:32 -0600

    tweaked comments

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -108,7 +108,6 @@ router.put('/:id', (req, res) => {
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
       const newProductTags = productTagIds
         // .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
@@ -117,12 +116,12 @@ router.put('/:id', (req, res) => {
             tag_id,
           };
         });
-      // figure out which ones to remove
+      // decide which needs removed
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !newProductTags.includes(tag_id))
         .map(({ id }) => id);
 
-      // run both actions
+      // run both
       return Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
